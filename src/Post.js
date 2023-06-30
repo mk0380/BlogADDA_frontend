178307b52be966fec543e15d8f2b357ef1e64299
mk0380 <123063925+mk0380@users.noreply.github.com>
2023-06-30 23:10:34 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -9,7 +9,7 @@ const Post = ({_id, title, summary, cover, content, createdAt, author }) => {
       <div className="post" >
         <div className="image">
           <Link to={`/post/${_id}`}>
-            <img src={'http://localhost:4000/' + cover} alt="" />
+            <img src={'https://blogadda-backend.onrender.com/' + cover} alt="" />
           </Link>
         </div>
         <div className="texts">
