c7c8484ce4d659594870aab7b499b7f4db1d2909
mk0380 <123063925+mk0380@users.noreply.github.com>
2023-06-30 23:14:32 +0530

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -10,7 +10,7 @@ export default function RegisterPage() {
 
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://blogadda-backend.onrender.com/register', {
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
