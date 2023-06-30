8680c3c150010d2d8e75d1624eceeedc15a9857f
mk0380 <123063925+mk0380@users.noreply.github.com>
2023-06-30 23:11:08 +0530

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -44,7 +44,7 @@ const CreatePost = () => {
         data.set('file', files);
 
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/post', {
+        const response = await fetch('https://blogadda-backend.onrender.com/post', {
             method: 'POST',
             body: data,
             credentials: 'include'
